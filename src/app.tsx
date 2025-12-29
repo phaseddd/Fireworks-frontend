1c21efd76a912ccc09eb9ddd5fd26a44f3fba26f
phaseddd <15016801384@163.com>
2025-12-29 13:39:28 +0800

    fix(weapp): polyfill DOM globals for NutUI transition

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,23 @@ import { PropsWithChildren } from 'react'
 import { useLaunch } from '@tarojs/taro'
 import './app.scss'
 
+function ensureDomGlobals() {
+  const globalObject = globalThis as unknown as Record<string, unknown>
+
+  if (typeof globalObject.Element === 'undefined') {
+    globalObject.Element = function Element() {}
+  }
+  if (typeof globalObject.HTMLElement === 'undefined') {
+    globalObject.HTMLElement = function HTMLElement() {}
+  }
+  if (typeof globalObject.Node === 'undefined') {
+    globalObject.Node = function Node() {}
+  }
+}
+
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
+    ensureDomGlobals()
     console.log('🎆 Fireworks App launched!')
   })
 
